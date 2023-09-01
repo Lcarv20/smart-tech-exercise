34b51f1eda14ea44521e15d75d38ddddb210f4d2
Luís Carvalho <42584819+Lcarv20@users.noreply.github.com>
2023-09-01 20:03:37 +0100

    Adding chart and small fixes

--- List.md
+++
@@ -1,5 +0,0 @@
-Need to:
-
-- List data on AG Grid for all routes
-- Get params (using react router), and perform a search
-- Delete, add, delete, update tags, posts and users
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,39 @@
-import { Button } from "@mui/material";
-import { useAppDispatch } from "../../stores/hooks";
-import { Severity, openSnackbar } from "../../stores/snackbarReducer";
+import { useEffect, useState } from "react";
+import { dataFetch } from "../../functions/requests";
+import { PieChart } from "@mui/x-charts/PieChart";
+import { Box } from "@mui/material";
 
 export default function Home() {
-  const dispatch = useAppDispatch();
-  const handleClick = () => {
-    dispatch(
-      openSnackbar({
-        open: true,
-        message: "This is a warning",
-        severity: Severity.warning,
-      }),
-    );
-  };
+  const [users, setUsers] = useState([]);
+  const [tags, setTags] = useState([]);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    dataFetch("Users").then((res) => {
+      setUsers(res);
+    });
+    dataFetch("Tags").then((res) => {
+      setTags(res);
+    });
+    dataFetch("Posts").then((res) => {
+      setPosts(res);
+    });
+  }, []);
   return (
-    <div>
-      Home
-      <Button onClick={handleClick}>TriggerSnack</Button>
-    </div>
+    <Box display={"flex"} height={"100%"} justifyContent={"center"} alignItems={"center"}>
+      <PieChart
+        width={600}
+        height={400}
+        series={[
+          {
+            data: [
+              { id: 0, value: users.length, label: "Users" },
+              { id: 1, value: posts.length, label: "Posts" },
+              { id: 2, value: tags.length, label: "Tags" },
+            ],
+          },
+        ]}
+      />
+    </Box>
   );
 }
